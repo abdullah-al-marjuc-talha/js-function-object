ce81d0f04cac7cc847dacfb6300aa6d8e1f7e587
Abdullah Al Marjuc Talha <abdullahalmarjuct@gmail.com>
2023-08-19 02:23:44 +0600

    object added

---
+++ object.js
@@ -0,0 +1,26 @@
+var student = {
+    id: 115, 
+    name: 'solaiman khan', 
+    class: 9, 
+    marks: 98
+}
+
+var mobile = {
+    brand : 'samsung',
+    price : 19000,
+    Storage : '64gb',
+    camera : '7mp'
+}
+
+var myComputer = {
+    brand : 'lonovo',
+    price : 39000,
+    color : 'silver',
+    processor : 'i7'
+}
+
+console.log(myComputer);
+myComputer.processor= 'i17';
+console.log(myComputer);
+console.log(myComputer.price);
+console.log(mobile);
