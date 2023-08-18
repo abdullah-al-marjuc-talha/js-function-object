68739269ac27deed727e13ebe90da669f0a502e1
Abdullah Al Marjuc Talha <abdullahalmarjuct@gmail.com>
2023-08-19 01:40:29 +0600

    more parameter added

--- parameter.js
+++ parameter.js
@@ -1,7 +1,31 @@
+// function bringSingara(money)
+// {
+//     console.log('mama singara den');
+//     console.log(100);
+// }
+
+// bringSingara(100);
+
 function bringSingara(money)
 {
-    console.log('mama singara den');
-    console.log(100);
+    console.log('ai nen singara');
+    console.log('eto taka disen:', money);
 }
 
-bringSingara(100);
+var taka= 200;
+// bringSingara(taka);
+
+function add(num1,num2)
+{
+    console.log('going to add: ', num1,num2)
+}
+// add(100,200);
+
+
+function sum(a,b,c,d,e)
+{
+    var sum=a+b+c+d+e;
+    console.log(sum);
+}
+
+sum(2,2,2,2,2);
